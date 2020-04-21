8f925b0649b9f8f2f0d174384d31fbe1ba4b8423
Charles Agbakwu <musiqe31@users.noreply.github.com>
2020-04-21 16:53:49 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5f2d87e948802f9d0dbdd1a416f115fd.js"
+  "/precache-manifest.e6c9638007b940b9a17a0039c1c06b3c.js"
 );
 
 self.addEventListener('message', (event) => {
